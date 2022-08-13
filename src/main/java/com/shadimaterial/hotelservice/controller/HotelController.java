d4afa74c47bf09179e0d9ad0bad5dab78093d69e
Deepak Patel <deepak.patel@gen-xt.com>
2022-08-13 22:28:14 +0530

    New Hotel added

--- src/main/java/com/shadimaterial/hotelservice/controller/HotelController.java
+++ src/main/java/com/shadimaterial/hotelservice/controller/HotelController.java
@@ -1,6 +1,7 @@
 package com.shadimaterial.hotelservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class HotelController {
     @GetMapping("/list")
     public String getHotel(){
         return "Result from Hotel Service and Docker";
+    }
+
+    @PostMapping("/add")
+    public String postHotel(){
+        return "Add new Hotel";
     }
 }
